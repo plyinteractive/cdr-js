83b4ec6070bf8517a32164c08af67d634ac764d2
Andrew Shenstone <andrew@plyinteractive.com>
2015-11-06 09:44:36 -0800

    Add ember library. Update bower file.

---
+++ examples/ember/app/app.js
@@ -0,0 +1,30 @@
+import Ember from 'ember';
+import Resolver from 'ember/resolver';
+import loadInitializers from 'ember/load-initializers';
+import config from './config/environment';
+
+var App;
+
+Ember.MODEL_FACTORY_INJECTIONS = true;
+
+new Cedar.Application({
+  server: config.apiURL,
+  fetch: true,
+  wait: true,
+  allowUnsecured: true,
+  objectNameFilter: "",
+  liveMode: true,
+  debug: false,
+  inline: true
+});
+
+
+App = Ember.Application.extend({
+  modulePrefix: config.modulePrefix,
+  podModulePrefix: config.podModulePrefix,
+  Resolver: Resolver
+});
+
+loadInitializers(App, config.modulePrefix);
+
+export default App;
